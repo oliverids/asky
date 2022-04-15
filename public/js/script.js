1e8a12261c8871835111458f2534dec361d446e4
Isabela Oliveira <oliver.isabela2303@gmail.com>
2022-04-15 00:04:10 -0300

    header and halfway through main section

--- public/js/script.js
+++ public/js/script.js
@@ -11,3 +11,22 @@ window.addEventListener('scroll', () => {
         topo = body.getBoundingClientRect().top;
     topo < -380 ? backtop.classList.add('show') : backtop.classList.remove('show')
 })
+
+const toggleMenu = document.getElementById('toggleMenu'),
+    asideMenu = document.getElementById('aside');
+
+toggleMenu.addEventListener('click', () => {
+    [asideMenu, toggleMenu].forEach(e => e.classList.toggle('ativo'))
+});
+
+window.addEventListener('click', e => {
+    if (asideMenu.classList.contains('ativo') && !asideMenu.contains(e.target) && !toggleMenu.contains(e.target)) {
+        [asideMenu, toggleMenu].forEach(e => e.classList.remove('ativo'))
+    }
+});
+
+const themeBtn = document.getElementById('theme');
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('escuro');
+    themeBtn.classList.toggle('ativo')
+})
