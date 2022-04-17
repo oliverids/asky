6b5e8d66240032046edb0ccfcfd5c15220e0bf40
Isabela Oliveira <oliver.isabela2303@gmail.com>
2022-04-17 18:17:49 -0300

    heroku info fix

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize'),
     connection = new Sequelize(process.env.DATABASE_URL, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-        host: 'localhost',
+        host: 'us-cdbr-east-05.cleardb.net',
         dialect: 'mysql'
     });
 
